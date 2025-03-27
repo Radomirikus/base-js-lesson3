725d44ffea689ff8cc78f141366a3bb15d429649
Radomirikus <r2a3d0o3m1i2r@gmail.com>
2025-03-27 17:40:50 +0300

    сделал вывод

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-let namee = 'олег'
-let surname = 'олегович'
-let second_surname = 'олегович'
-
-console.log(namee + surname + second_surname)
-
 let date = document.getElementById('date')
 date.addEventListener('change', function (){
-    console.log('нажал')
-    console.log(date.day.value)
-    console.log(date.month.value)
-    console.log(date.year.value)
+    let answer = document.getElementById('answer')
+    answer.innerHTML = `${date.day.value} ${date.month.value} ${date.year.value}`
 })
+
